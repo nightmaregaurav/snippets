b74ffe873c67606313b018426bc917938a8cfb47
Gaurav Nyaupane <mail.gauravnyaupane@gmail.com>
2022-08-09 14:45:08 +0545

    cleanup

--- Django_boilerplate/Core/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import SiteSetting
-
-
-@admin.register(SiteSetting)
-class SiteSettingAdmin(admin.ModelAdmin):
-    def get_list_display(self, request):
-        return 'key', 'value'
--- JavaSnippets/JSP/TestJsp.jsp
+++
@@ -1,8 +0,0 @@
-<html>
-    <head>
-        <title>JSP Page</title>
-    </head>
-    <body>
-        <h1><% out.print("Hello world"); %></h1>
-    </body>
-</html>
