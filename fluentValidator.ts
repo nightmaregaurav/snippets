b06244987da9f5d642e5ceb1754d326962001b8a
Gaurav Nyaupane <84658229+nightmaregaurav@users.noreply.github.com>
2023-08-03 11:05:55 +0545

    Update fluentValidator.ts

--- fluentValidator.ts
+++ fluentValidator.ts
@@ -1,251 +1,300 @@
 class Ensure<T> {
-  private value: T;
-  private errors: string[];
+    private readonly value: T;
+    private readonly errors: string[];
 
-  constructor(value: T, prevErrors: string[] = []) {
-    this.value = value;
-    this.errors = prevErrors;
-  }
+    constructor(value: T, prevErrors: string[] = []) {
+        this.value = value;
+        this.errors = prevErrors;
+    }
 
-  public static given<T>(value: T): Ensure<T> {
-    return new Ensure<T>(value);
-  }
+    public static given<T>(value: T, prevErrors: string[] = []): Ensure<T> {
+        return new Ensure<T>(value, prevErrors);
+    }
 
-  public peek<TT>(transform: (value: T) => TT): Ensure<TT> {
-    const newValue = transform(this.value);
-    return Ensure.given<TT>(newValue, [...this.errors]);
-  }
+    public peek<TT>(transform: (value: T) => TT): Ensure<TT> {
+        const newValue = transform(this.value);
+        return Ensure.given<TT>(newValue, [...this.errors]);
+    }
 
-  public isNullish(): Validate {
-    if (this.value !== null && this.value !== undefined) {
-      this.errors.push('Value must be null or undefined.');
+    public passesTest(testName: string, test: (value: T) => boolean): Ensure<T> {
+        if (!test(this.value)) {
+            this.errors.push(`Value must pass test "${testName}".`);
+        }
+        return this;
     }
-    return this;
-  }
 
-  public isNotNullish(): Validate {
-    if (this.value === null || this.value === undefined) {
-      this.errors.push('Value must not be null or undefined.');
+    public isNullish(): Ensure<T> {
+        if (this.value !== null && this.value !== undefined) {
+            this.errors.push('Value must be null or undefined.');
+        }
+        return this;
     }
-    return this;
-  }
 
-  public isString(): Validate {
-    if (typeof this.value !== 'string') {
-      this.errors.push('Value must be a string.');
+    public isNotNullish(): Ensure<T> {
+        if (this.value === null || this.value === undefined) {
+            this.errors.push('Value must not be null or undefined.');
+        }
+        return this;
     }
-    return this;
-  }
 
-  public isNotString(): Validate {
-    if (typeof this.value === 'string') {
-      this.errors.push('Value must not be a string.');
+    public isString(): Ensure<T> {
+        if (typeof this.value !== 'string') {
+            this.errors.push('Value must be a string.');
+        }
+        return this;
     }
-    return this;
-  }
 
-  public isNumeric(): Validate {
-    if (!/^-?\d*\.?\d+$/.test(this.value)) {
-      this.errors.push('Value must be numeric.');
+    public isNotString(): Ensure<T> {
+        if (typeof this.value === 'string') {
+            this.errors.push('Value must not be a string.');
+        }
+        return this;
     }
-    return this;
-  }
 
-  public isNotNumeric(): Validate {
-    if (/^-?\d*\.?\d+$/.test(this.value)) {
-      this.errors.push('Value must not be numeric.');
+    public isNumeric(): Ensure<T> {
+        if (typeof this.value !== 'string'){
+            throw Error("isNumeric can only be applied to strings.");
+        }
+        if (!/^-?\d*\.?\d+$/.test(this.value)) {
+            this.errors.push('Value must be numeric.');
+        }
+        return this;
     }
-    return this;
-  }
 
-  public isNumber(): Validate {
-    if (typeof this.value !== 'number' || isNaN(this.value)) {
-      this.errors.push('Value must be a valid number.');
-    }
-    return this;
-  }
+    public isNotNumeric(): Ensure<T> {
+        if (typeof this.value !== 'string') {
+            throw Error("isNotNumeric can only be applied to strings.");
+        }
 
-  public isNotNumber(): Validate {
-    if (typeof this.value === 'number' && !isNaN(this.value)) {
-      this.errors.push('Value must not be a valid number.');
+        if (/^-?\d*\.?\d+$/.test(this.value)) {
+            this.errors.push('Value must not be numeric.');
+        }
+        return this;
     }
-    return this;
-  }
 
-  public isObject(): Validate {
-    if (typeof this.value !== 'object' || this.value === null || Array.isArray(this.value)) {
-      this.errors.push('Value must be an object.');
+    public isNumber(): Ensure<T> {
+        if (typeof this.value !== 'number') {
+            this.errors.push('Value must be a valid number.');
+        }
+        return this;
     }
-    return this;
-  }
 
-  public isNotObject(): Validate {
-    if (typeof this.value === 'object' && this.value !== null && !Array.isArray(this.value)) {
-      this.errors.push('Value must not be an object.');
+    public isNotNumber(): Ensure<T> {
+        if (typeof this.value === 'number') {
+            this.errors.push('Value must not be a valid number.');
+        }
+        return this;
     }
-    return this;
-  }
 
-  public isArray(): Validate {
-    if (!Array.isArray(this.value)) {
-      this.errors.push('Value must be an array.');
+    public isObject(): Ensure<T> {
+        if (typeof this.value !== 'object' || this.value === null || Array.isArray(this.value)) {
+            this.errors.push('Value must be an object.');
+        }
+        return this;
     }
-    return this;
-  }
 
-  public isNotArray(): Validate {
-    if (Array.isArray(this.value)) {
-      this.errors.push('Value must not be an array.');
+    public isNotObject(): Ensure<T> {
+        if (typeof this.value === 'object' && this.value !== null && !Array.isArray(this.value)) {
+            this.errors.push('Value must not be an object.');
+        }
+        return this;
     }
-    return this;
-  }
 
-  public isLessThan(limit: number | string): Validate {
-    if (typeof this.value !== 'number' && typeof this.value !== 'string') {
-      throw new Error('isLessThan can only be applied to numbers and strings.');
+    public isArray(): Ensure<T> {
+        if (!Array.isArray(this.value)) {
+            this.errors.push('Value must be an array.');
+        }
+        return this;
     }
-    if (this.value >= limit) {
-      this.errors.push(`Value must be less than ${limit}.`);
-    }
-    return this;
-  }
 
-  public isNotLessThan(limit: number | string): Validate {
-    if (typeof this.value !== 'number' && typeof this.value !== 'string') {
-      throw new Error('isNotLessThan can only be applied to numbers and strings.');
+    public isNotArray(): Ensure<T> {
+        if (Array.isArray(this.value)) {
+            this.errors.push('Value must not be an array.');
+        }
+        return this;
     }
-    if (this.value < limit) {
-      this.errors.push(`Value must not be less than ${limit}.`);
-    }
-    return this;
-  }
 
-  public isMoreThan(limit: number | string): Validate {
-    if (typeof this.value !== 'number' && typeof this.value !== 'string') {
-      throw new Error('isMoreThan can only be applied to numbers and strings.');
+    public isLessThan(limit: number | string): Ensure<T> {
+        if (typeof this.value !== 'number' && typeof this.value !== 'string') {
+            throw new Error('isLessThan can only be applied to numbers and strings.');
+        }
+        if (this.value >= limit) {
+            this.errors.push(`Value must be less than ${limit}.`);
+        }
+        return this;
     }
-    if (this.value <= limit) {
-      this.errors.push(`Value must be greater than ${limit}.`);
-    }
-    return this;
-  }
 
-  public isNotMoreThan(limit: number | string): Validate {
-    if (typeof this.value !== 'number' && typeof this.value !== 'string') {
-      throw new Error('isNotMoreThan can only be applied to numbers and strings.');
+    public isNotLessThan(limit: number | string): Ensure<T> {
+        if (typeof this.value !== 'number' && typeof this.value !== 'string') {
+            throw new Error('isNotLessThan can only be applied to numbers and strings.');
+        }
+        if (this.value < limit) {
+            this.errors.push(`Value must not be less than ${limit}.`);
+        }
+        return this;
     }
-    if (this.value > limit) {
-      this.errors.push(`Value must not be greater than ${limit}.`);
-    }
-    return this;
-  }
 
-  public startsWith(substring: string): Validate {
-    if (typeof this.value !== 'string' && !Array.isArray(this.value)) {
-      throw new Error('startsWith can only be applied to strings and arrays.');
+    public isGreaterThan(limit: number | string): Ensure<T> {
+        if (typeof this.value !== 'number' && typeof this.value !== 'string') {
+            throw new Error('isGreaterThan can only be applied to numbers and strings.');
+        }
+        if (this.value <= limit) {
+            this.errors.push(`Value must be greater than ${limit}.`);
+        }
+        return this;
     }
-    if (!this.value.startsWith(substring)) {
-      this.errors.push(`Value must start with the substring '${substring}'.`);
-    }
-    return this;
-  }
 
-  public doesNotStartWith(substring: string): Validate {
-    if (typeof this.value === 'string' && !Array.isArray(this.value)) {
-      if (this.value.startsWith(substring)) {
-        this.errors.push(`Value must not start with the substring '${substring}'.`);
-      }
-    } else {
-      throw new Error('doesNotStartWith can only be applied to strings and arrays.');
+    public isNotGreaterThan(limit: number | string): Ensure<T> {
+        if (typeof this.value !== 'number' && typeof this.value !== 'string') {
+            throw new Error('isNotGreaterThan can only be applied to numbers and strings.');
+        }
+        if (this.value > limit) {
+            this.errors.push(`Value must not be greater than ${limit}.`);
+        }
+        return this;
     }
-    return this;
-  }
 
-  public endsWith(substring: string): Validate {
-    if (typeof this.value !== 'string' && !Array.isArray(this.value)) {
-      throw new Error('endsWith can only be applied to strings and arrays.');
+    public startsWith(substring: string): Ensure<T> {
+        if (typeof this.value !== 'string') {
+            throw new Error('startsWith can only be applied to strings.');
+        }
+        if (!this.value.startsWith(substring)) {
+            this.errors.push(`Value must start with the substring '${substring}'.`);
+        }
+        return this;
     }
-    if (!this.value.endsWith(substring)) {
-      this.errors.push(`Value must end with the substring '${substring}'.`);
-    }
-    return this;
-  }
 
-  public doesNotEndWith(substring: string): Validate {
-    if (typeof this.value === 'string' && !Array.isArray(this.value)) {
-      if (this.value.endsWith(substring)) {
-        this.errors.push(`Value must not end with the substring '${substring}'.`);
-      }
-    } else {
-      throw new Error('doesNotEndWith can only be applied to strings and arrays.');
+    public doesNotStartWith(substring: string): Ensure<T> {
+        if (typeof this.value !== 'string') {
+            throw new Error('doesNotStartWith can only be applied to strings.');
+        }
+        if (this.value.startsWith(substring)) {
+            this.errors.push(`Value must not start with the substring '${substring}'.`);
+        }
+        return this;
     }
-    return this;
-  }
 
-  public equals(expectedValue: any): Validate {
-    if (this.value !== expectedValue) {
-      this.errors.push(`Value must be equal to ${expectedValue}.`);
+    public endsWith(substring: string): Ensure<T> {
+        if (typeof this.value !== 'string') {
+            throw new Error('endsWith can only be applied to strings.');
+        }
+        if (!this.value.endsWith(substring)) {
+            this.errors.push(`Value must end with the substring '${substring}'.`);
+        }
+        return this;
     }
-    return this;
-  }
 
-  public isNotEqual(expectedValue: any): Validate {
-    if (this.value === expectedValue) {
-      this.errors.push(`Value must not be equal to ${expectedValue}.`);
+    public doesNotEndWith(substring: string): Ensure<T> {
+        if (typeof this.value !== 'string') {
+            throw new Error('doesNotEndWith can only be applied to strings.');
+        }
+        if (this.value.endsWith(substring)) {
+            this.errors.push(`Value must not end with the substring '${substring}'.`);
+        }
+        return this;
     }
-    return this;
-  }
 
-  public matchesRegex(regex: RegExp): Validate {
-    if (!regex.test(this.value)) {
-      this.errors.push('Value does not match the expected format.');
+    public equals(expectedValue: any): Ensure<T> {
+        if (this.value !== expectedValue) {
+            this.errors.push(`Value must be equal to ${expectedValue}.`);
+        }
+        return this;
     }
-    return this;
-  }
 
-  public doesNotMatchRegex(regex: RegExp): Validate {
-    if (regex.test(this.value)) {
-      this.errors.push('Value must not match the expected format.');
+    public isNotEqual(expectedValue: any): Ensure<T> {
+        if (this.value === expectedValue) {
+            this.errors.push(`Value must not be equal to ${expectedValue}.`);
+        }
+        return this;
     }
-    return this;
-  }
 
-  public contains(substring: string): Validate {
-    if (typeof this.value !== 'string' || this.value.indexOf(substring) === -1) {
-      this.errors.push(`Value must contain the substring '${substring}'.`);
+    public matchesRegex(regex: RegExp): Ensure<T> {
+        if (typeof this.value !== 'string') {
+            throw new Error('matchesRegex can only be applied to strings.');
+        }
+        if (!regex.test(this.value)) {
+            this.errors.push('Value does not match the expected format.');
+        }
+        return this;
     }
-    return this;
-  }
 
-  public doesNotContain(substring: string): Validate {
-    if (typeof this.value === 'string' && this.value.indexOf(substring) !== -1) {
-      this.errors.push(`Value must not contain the substring '${substring}'.`);
+    public doesNotMatchRegex(regex: RegExp): Ensure<T> {
+        if (typeof this.value !== 'string') {
+            throw new Error('doesNotMatchRegex can only be applied to strings.');
+        }
+        if (regex.test(this.value)) {
+            this.errors.push('Value must not match the expected format.');
+        }
+        return this;
     }
-    return this;
-  }
 
-  public hasLength(length: number): Validate {
-    if (typeof this.value !== 'string' && !Array.isArray(this.value)) {
-      throw new Error('hasLength can only be applied to strings and arrays.');
+    public contains(value: T): Ensure<T> {
+        if (typeof this.value !== 'string' && !Array.isArray(this.value)) {
+            throw new Error('contains can only be applied to strings and arrays.');
+        }
+        if (typeof this.value !== typeof value) {
+            throw new Error('contains can only be applied to same types.');
+        }
+        if (this.value.indexOf(value as any) === -1) {
+            this.errors.push(`Value must contain '${value}'.`);
+        }
+        return this;
     }
-    if (this.value.length !== length) {
-      this.errors.push(`Value must have a length of ${length}.`);
-    }
-    return this;
-  }
 
-  public doesNotHaveLength(length: number): Validate {
-    if (typeof this.value === 'string' || Array.isArray(this.value)) {
-      if (this.value.length === length) {
-        this.errors.push(`Value must not have a length of ${length}.`);
-      }
-    } else {
-      throw new Error('doesNotHaveLength can only be applied to strings and arrays.');
+    public doesNotContain(value: T): Ensure<T> {
+        if (typeof this.value !== 'string' && !Array.isArray(this.value)) {
+            throw new Error('doesNotContain can only be applied to strings and arrays.');
+        }
+        if (typeof this.value !== typeof value) {
+            throw new Error('doesNotContain can only be applied to same types.');
+        }
+        if (this.value.indexOf(value as any) !== -1) {
+            this.errors.push(`Value must not contain '${value}'.`);
+        }
+        return this;
     }
-    return this;
-  }
 
-  public evaluate(): { isValid: boolean; errors: string[] } {
-    return { isValid: this.errors.length === 0, errors: this.errors };
-  }
+    public hasLength(length: number): Ensure<T> {
+        if (typeof this.value !== 'string' && !Array.isArray(this.value)) {
+            throw new Error('hasLength can only be applied to strings and arrays.');
+        }
+        if (this.value.length !== length) {
+            this.errors.push(`Value must have a length of ${length}.`);
+        }
+        return this;
+    }
+
+    public doesNotHaveLength(length: number): Ensure<T> {
+        if (typeof this.value !== 'string' && !Array.isArray(this.value)) {
+            throw new Error('doesNotHaveLength can only be applied to strings and arrays.');
+        }
+        if (this.value.length === length) {
+            this.errors.push(`Value must not have a length of ${length}.`);
+        }
+        return this;
+    }
+
+    public hasAtLeastLength(length: number): Ensure<T> {
+        if (typeof this.value !== 'string' && !Array.isArray(this.value)) {
+            throw new Error('hasAtLeastLength can only be applied to strings and arrays.');
+        }
+        if (this.value.length >= length) {
+            this.errors.push(`Value must have a length of ${length} at minimum.`);
+        }
+        return this;
+    }
+
+    public hasAtMostLength(length: number): Ensure<T> {
+        if (typeof this.value !== 'string' && !Array.isArray(this.value)) {
+            throw new Error('hasAtMostLength can only be applied to strings and arrays.');
+        }
+        if (this.value.length <= length) {
+            this.errors.push(`Value must have a length of ${length} at maximum.`);
+        }
+        return this;
+    }
+
+    public evaluate(): { isValid: boolean; errors: string[] } {
+        return {isValid: this.errors.length === 0, errors: this.errors};
+    }
 }
